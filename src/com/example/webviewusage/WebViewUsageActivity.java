b98d8dc312ccfcc32de13582b94fd4dd08990eab
Abbas <abbas.s786@yahoo.co.in>
2015-04-27 09:03:11 +0530

    Initial android project commit

---
+++ src/com/example/webviewusage/WebViewUsageActivity.java
@@ -0,0 +1,15 @@
+package com.example.webviewusage;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class WebViewUsageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_web_view_usage);
+	}
+}
