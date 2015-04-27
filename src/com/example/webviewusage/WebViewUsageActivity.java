971618131271caa27dad8221995efbdc2f4d556e
Abbas <abbas.s786@yahoo.co.in>
2015-04-27 09:28:22 +0530

    Added mail activity which will have buttons to launch various webview example activity

--- src/com/example/webviewusage/WebViewUsageActivity.java
+++ src/com/example/webviewusage/WebViewUsageActivity.java
@@ -3,6 +3,7 @@ package com.example.webviewusage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class WebViewUsageActivity extends Activity {
@@ -11,5 +12,16 @@ public class WebViewUsageActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_web_view_usage);
+	}
+	
+	public void onBtnClicked(View v){
+		
+		switch (v.getId()) {
+			case R.id.btnWebViewActivityWithURL:
+				break;
+			case R.id.btnWebViewActivityWithHTML:
+				break;
+		}
+	  
 	}
 }
