7aa7981dde18db366308fc467700d9b88ac08364
Abbas <abbas.s786@yahoo.co.in>
2015-05-01 15:06:46 +0530

    Corrected indentation

--- src/com/example/webviewusage/WebVievActivityWithURL.java
+++ src/com/example/webviewusage/WebVievActivityWithURL.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 public class WebVievActivityWithURL extends Activity {
-	
-	private WebView myWebView;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_web_view_with_url);
-		
-		myWebView = (WebView) findViewById(R.id.webview);
-		
-		myWebView.loadUrl("https://m.facebook.com");
-	}
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_with_url);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView.loadUrl("https://m.facebook.com");
+    }
 
 }
--- src/com/example/webviewusage/WebViewUsageActivity.java
+++ src/com/example/webviewusage/WebViewUsageActivity.java
@@ -1,30 +1,28 @@
 package com.example.webviewusage;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
 public class WebViewUsageActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_web_view_usage);
-	}
-	
-	public void onBtnClicked(View v){
-		
-		switch (v.getId()) {
-			case R.id.btnWebViewActivityWithURL:
-				startActivity(new Intent(this, WebVievActivityWithURL.class));
-				break;
-			case R.id.btnWebViewActivityWithHTML:
-				startActivity(new Intent(this, WebViewActivityWithHTML.class));
-			break;
-		}
-	  
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_usage);
+    }
+
+    public void onBtnClicked(View v) {
+
+        switch (v.getId()) {
+        case R.id.btnWebViewActivityWithURL:
+            startActivity(new Intent(this, WebVievActivityWithURL.class));
+            break;
+        case R.id.btnWebViewActivityWithHTML:
+            startActivity(new Intent(this, WebViewActivityWithHTML.class));
+            break;
+        }
+
+    }
 }
